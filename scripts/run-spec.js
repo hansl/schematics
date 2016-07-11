355b94f5b73aaefc246a9e3f5c87aedf2819634b
Hans Larsen <hans@hansl.ca>
2016-07-10 19:18:45 -0700

    Removing spec reporter.

--- scripts/run-spec.js
+++ scripts/run-spec.js
@@ -3,7 +3,6 @@
 
 const path = require("path");
 const Jasmine = require("jasmine");
-const JasmineSpecReporter = require('jasmine-spec-reporter');
 
 
 const projectBaseDir = '../dist';
@@ -12,7 +11,6 @@ const projectBaseDir = '../dist';
 // Create a Jasmine runner and configure it.
 const jasmine = new Jasmine({ projectBaseDir: path.join(__dirname, projectBaseDir) });
 jasmine.loadConfig({});
-jasmine.addReporter(new JasmineSpecReporter({}));
 
 // Run the tests.
 jasmine.execute([ path.join(projectBaseDir, '**/*.spec.js') ]);
