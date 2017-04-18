5c4134e1355b76cccdda86155be40dd888b547cc
Filipe Silva <filipematossilva@gmail.com>
2017-04-18 22:51:56 +0100

    remove old lib

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,3 @@ export * from './low-level/entry';
 export * from './low-level/sink';
 export * from './low-level/source';
 export * from './low-level/transform';
-
-// export * from './schematics/library';
-// export * from './schematics/schematics';
