5acb541e8a095851d2027c05e77d548c2fc0e1d8
Filipe Silva <filipematossilva@gmail.com>
2017-04-19 13:28:25 +0100

    restore file

---
+++ src/low-level/fs.ts
@@ -0,0 +1,76 @@
+import fs = require('fs');
+
+import {FileSystemException} from './source';
+
+
+export function readFile(p: string, encoding: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(p, encoding, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+export function writeFile(p: string, data: string, encoding: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(p, data, encoding, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export function mkdir(p: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(p, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export function readdir(p: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(p, (err, files) => {
+      if (err) {
+        reject(new FileSystemException(err));
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
+
+export function access(p: string, mode: number): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fs.access(p, mode, (err) => {
+      if (err) {
+        reject(new FileSystemException(err));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export function stat(p: string): Promise<fs.Stats> {
+  return new Promise((resolve, reject) => {
+    fs.stat(p, (err, stats) => {
+      if (err) {
+        reject(new FileSystemException(err));
+      } else {
+        resolve(stats);
+      }
+    });
+  });
+}
