44099507dd13485394c272f1471a34efc0e62de7
Filipe Silva <filipematossilva@gmail.com>
2017-04-19 13:09:05 +0100

    add comment

--- src/exception.ts
+++ src/exception.ts
@@ -1,2 +1,5 @@
 
+// Starting with TS 2.1, Error cannot be properly extended anymore. Need to find a better way.
+// https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+// export class BaseException extends Error{}
 export class BaseException {}
