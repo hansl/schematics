f67c7c55b308a963be6e1d161a20575260a5369b
Filipe Silva <filipematossilva@gmail.com>
2017-04-19 16:14:19 +0100

    add real binary

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+
 import * as yargs from 'yargs';
 import * as path from 'path';
+import { existsSync } from 'fs';
 import { Observable } from 'rxjs/Observable';
 
 import {
@@ -15,6 +18,34 @@ import 'rxjs/add/operator/let';
 import 'rxjs/add/operator/do';
 
 
+export function findUp(names: string | string[], from: string, stopOnNodeModules = false) {
+  if (!Array.isArray(names)) {
+    names = [names];
+  }
+  const root = path.parse(from).root;
+
+  let currentDir = from;
+  while (currentDir && currentDir !== root) {
+    for (const name of names) {
+      const p = path.join(currentDir, name);
+      if (existsSync(p)) {
+        return p;
+      }
+    }
+
+    if (stopOnNodeModules) {
+      const nodeModuleP = path.join(currentDir, 'node_modules');
+      if (existsSync(nodeModuleP)) {
+        return null;
+      }
+    }
+
+    currentDir = path.dirname(currentDir);
+  }
+
+  return null;
+}
+
 function usage(collection: Collection) {
   console.log(`
     ERROR: You must provide a blueprint name e.g.: "schem blueprint-name --flag=value".
@@ -26,14 +57,18 @@ function usage(collection: Collection) {
 }
 
 export default function cli() {
+  // Compute paths.
   const cwd = process.cwd();
+  const projectRoot = path.dirname(findUp('package.json', cwd, true));
+
+  // Parse args.
   let { _: nonFlags, collection: collectionPath, dryRun, ...options } = yargs.argv;
   collectionPath = collectionPath || './schematics/';
   // Delete binary path entry.
   delete options['$0'];
 
   // Load collection.
-  const collection = new Collection({ path: path.resolve(cwd, collectionPath) });
+  const collection = new Collection({ path: path.resolve(projectRoot, collectionPath) });
 
   // Load blueprint.
   if (nonFlags.length !== 1) {
